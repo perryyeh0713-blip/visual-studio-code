791094215573a0747a306e03a41cdd2a6e559697
perryyeh0713-blip <perryyeh0713@gmail.com>
2026-09-17 15:19:51 +0800

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Hello World!\n";
+
+    int a=5;
+    float b=0.03;
+
+    cout << "a=" << a << endl;
+    cout << "b=" << b << endl;
+}
